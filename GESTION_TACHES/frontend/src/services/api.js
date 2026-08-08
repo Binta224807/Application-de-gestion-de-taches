b5d214deec16649eb4ecf8f2be085d0bc9b15a3c
Binta <m46793575@gmail.com>
2026-08-08 22:20:22 +0200

    Correction de l'URL API en production

--- GESTION_TACHES/frontend/src/services/api.js
+++ GESTION_TACHES/frontend/src/services/api.js
@@ -1,49 +1,30 @@
 import axios from "axios";
 
-
 const api = axios.create({
-
-  baseURL: "http://127.0.0.1:8000/api/",
-
+  baseURL: "/api/",
   headers: {
     "Content-Type": "application/json",
   },
-
 });
 
-
-
 api.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem("access") ||
+      localStorage.getItem("access_token");
 
-(config) => {
+    console.log("URL :", config.url);
+    console.log("TOKEN :", token);
 
-  const token =
-    localStorage.getItem("access") ||
-    localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-
-  console.log("URL :", config.url);
-  console.log("TOKEN :", token);
-
-
-  if (token) {
-
-    config.headers.Authorization = `Bearer ${token}`;
-
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-
-  return config;
-
-},
-
-(error) => {
-
-  return Promise.reject(error);
-
-}
-
 );
-
 
 export default api;
